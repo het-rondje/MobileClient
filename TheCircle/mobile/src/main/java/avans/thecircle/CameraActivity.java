9cfde3d5eae87b966c23af1d922d63dbd6a3a256
Jorrit Meeuwissen <jorritjeah@gmail.com>
2019-06-12 14:08:14 +0200

    Added camera streaming

--- TheCircle/mobile/src/main/java/avans/thecircle/CameraActivity.java
+++ TheCircle/mobile/src/main/java/avans/thecircle/CameraActivity.java
@@ -1,4 +1,213 @@
 package avans.thecircle;
 
-public class CameraActivity {
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.pedro.encoder.input.video.CameraOpenException;
+import com.pedro.rtplibrary.rtmp.RtmpCamera1;
+import avans.thecircle.R;;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import net.ossrs.rtmp.ConnectCheckerRtmp;
+
+public class CameraActivity extends AppCompatActivity
+        implements ConnectCheckerRtmp, View.OnClickListener, SurfaceHolder.Callback {
+
+    private RtmpCamera1 rtmpCamera1;
+    private Button button;
+    private Button bRecord;
+    private EditText etUrl;
+
+    private String currentDateAndTime = "";
+    private File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+            + "/rtmp-rtsp-stream-client-java");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_camera);
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        button = findViewById(R.id.b_start_stop);
+        button.setOnClickListener(this);
+        bRecord = findViewById(R.id.b_record);
+        bRecord.setOnClickListener(this);
+        Button switchCamera = findViewById(R.id.switch_camera);
+        switchCamera.setOnClickListener(this);
+        etUrl = findViewById(R.id.et_rtp_url);
+        etUrl.setHint("URL");
+        rtmpCamera1 = new RtmpCamera1(surfaceView, this);
+        rtmpCamera1.setReTries(10);
+        surfaceView.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuccessRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(CameraActivity.this, "Connection success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailedRtmp(final String reason) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (rtmpCamera1.shouldRetry(reason)) {
+                    Toast.makeText(CameraActivity.this, "Retry", Toast.LENGTH_SHORT)
+                            .show();
+                    rtmpCamera1.reTry(5000);  //Wait 5s and retry connect stream
+                } else {
+                    Toast.makeText(CameraActivity.this, "Connection failed. " + reason, Toast.LENGTH_SHORT)
+                            .show();
+                    rtmpCamera1.stopStream();
+                    button.setText("Start streaming");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnectRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(CameraActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onAuthErrorRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(CameraActivity.this, "Auth error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onAuthSuccessRtmp() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(CameraActivity.this, "Auth success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.b_start_stop:
+                if (!rtmpCamera1.isStreaming()) {
+                    if (rtmpCamera1.isRecording()
+                            || rtmpCamera1.prepareAudio() && rtmpCamera1.prepareVideo()) {
+                        button.setText("Stop streaming");
+                        rtmpCamera1.startStream(etUrl.getText().toString());
+                    } else {
+                        Toast.makeText(this, "Error preparing stream, This device cant do it",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    button.setText("Start streaming");
+                    rtmpCamera1.stopStream();
+                }
+                break;
+            case R.id.switch_camera:
+                try {
+                    rtmpCamera1.switchCamera();
+                } catch (CameraOpenException e) {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.b_record:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    if (!rtmpCamera1.isRecording()) {
+                        try {
+                            if (!folder.exists()) {
+                                folder.mkdir();
+                            }
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+                            currentDateAndTime = sdf.format(new Date());
+                            if (!rtmpCamera1.isStreaming()) {
+                                if (rtmpCamera1.prepareAudio() && rtmpCamera1.prepareVideo()) {
+                                    rtmpCamera1.startRecord(
+                                            folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
+                                    bRecord.setText("Stop recording");
+                                    Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(this, "Error preparing stream, This device cant do it",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                rtmpCamera1.startRecord(
+                                        folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
+                                bRecord.setText("Stop recording");
+                                Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (IOException e) {
+                            rtmpCamera1.stopRecord();
+                            bRecord.setText("Start recording");
+                            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        rtmpCamera1.stopRecord();
+                        bRecord.setText("Start recording");
+                        Toast.makeText(this,
+                                "file " + currentDateAndTime + ".mp4 saved in " + folder.getAbsolutePath(),
+                                Toast.LENGTH_SHORT).show();
+                        currentDateAndTime = "";
+                    }
+                } else {
+                    Toast.makeText(this, "You need min JELLY_BEAN_MR2(API 18) for do it...",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        rtmpCamera1.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && rtmpCamera1.isRecording()) {
+            rtmpCamera1.stopRecord();
+            bRecord.setText("Start recording");
+            Toast.makeText(this,
+                    "file " + currentDateAndTime + ".mp4 saved in " + folder.getAbsolutePath(),
+                    Toast.LENGTH_SHORT).show();
+            currentDateAndTime = "";
+        }
+        if (rtmpCamera1.isStreaming()) {
+            rtmpCamera1.stopStream();
+            button.setText("Start streaming");
+        }
+        rtmpCamera1.stopPreview();
+    }
 }
